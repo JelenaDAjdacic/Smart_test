2c8fc2cabb8f5be37f80a7d26150ea3078660b2a
Jelena <ajdacic.d.jelena@gmail.com>
2016-01-25 18:36:31 +0100

    test

--- app/src/main/java/com/example/jelena/smart_test/MainActivity.java
+++ app/src/main/java/com/example/jelena/smart_test/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jelena.smart_test;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text= (TextView) findViewById(R.id.text);
+
+        Typeface face;
+        face = Typeface.createFromAsset(getAssets(), "fonts/AmsiPro-Bold.otf");
+
+        text.setTypeface(face);
     }
 }
